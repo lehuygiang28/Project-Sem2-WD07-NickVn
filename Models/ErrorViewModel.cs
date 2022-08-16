20d897222b1201629a50af10cbd569a2410af077
lehuygiang28 <lehuygiang28@gmail.com>
2022-08-16 15:51:42 +0700

    Init asp.net mvc template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Project_Sem2_VTCA_WD07_MuaAcc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
