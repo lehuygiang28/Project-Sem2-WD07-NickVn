900dfa14834685ab69af7ee986cdd4bf23cc90fa
lehuygiang28 <lehuygiang28@gmail.com>
2022-08-30 15:05:56 +0700

    Add database, models

---
+++ Models/NickVn_ProjectContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Project_Sem2_WD07_NickVn.Models
+{
+    public partial class NickVn_ProjectContext : DbContext
+    {
+        public NickVn_ProjectContext()
+        {
+        }
+
+        public NickVn_ProjectContext(DbContextOptions<NickVn_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("name=ConnectionStrings:NickVn_Project", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.22-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasColumnType("text")
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FirstName)
+                    .HasColumnType("text")
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.LastName)
+                    .HasColumnType("text")
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.Password)
+                    .HasColumnType("text")
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Role)
+                    .HasColumnType("int(11)")
+                    .HasColumnName("role")
+                    .HasDefaultValueSql("'1'");
+
+                entity.Property(e => e.UserName)
+                    .HasColumnType("text")
+                    .HasColumnName("user_name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_Sem2_WD07_NickVn.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int Role { get; set; }
+    }
+}
