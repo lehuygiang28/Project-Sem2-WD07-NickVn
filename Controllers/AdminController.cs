32c11fbef0e1c79bfc876934aec94586e143e824
lehuygiang28 <lehuygiang28@gmail.com>
2022-09-08 02:33:50 +0700

    Add view of admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Project_Sem2_WD07_NickVn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_Sem2_WD07_NickVn.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ILogger<AdminController> _logger;
+    private readonly NickVn_ProjectContext _context;
+
+    public AdminController(ILogger<AdminController> logger, NickVn_ProjectContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
