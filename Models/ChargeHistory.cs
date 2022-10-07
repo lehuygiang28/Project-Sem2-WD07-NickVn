66dac4f990cd80f4d0673c8a6289a0808f7389f7
lehuygiang28 <lehuygiang28@gmail.com>
2022-10-08 03:07:43 +0700

    Add charge history models

---
+++ Models/ChargeHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_Sem2_WD07_NickVn.Models
+{
+    public partial class ChargeHistory
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Telecom { get; set; } = null!;
+        public string Pin { get; set; } = null!;
+        public string Serial { get; set; } = null!;
+        public string TypeCharge { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public decimal MoneyReceived { get; set; }
+        public string? Note { get; set; }
+        public string Result { get; set; } = null!;
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
